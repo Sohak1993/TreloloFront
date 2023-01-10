63bf2ffcfb2f3cbd96ad19f87725f207d3bd6d24
Mehdy Maessckal <mehdy.maessckal@gmail.com>
2023-01-10 16:45:24 +0100

    Mardi 16h45

---
+++ src/app/models/User/connectedUser-model.ts
@@ -0,0 +1,12 @@
+
+export class ConnectedUser {
+  id: number;
+  email: string;
+  token : string;
+
+  constructor(id : number, email : string, token: string){
+      this.id = id;
+      this.email = email;
+      this.token = token;
+  }
+}
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,14 +1,40 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ConnectedUser } from '../models/User/connectedUser-model';
+import { Login } from '../models/User/login-model';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private _http: HttpClient) { }
+  //Observable connection
+  private _isConnected$ : BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  isConnected$ : Observable<boolean> = this._isConnected$.asObservable();
+
+  constructor(
+    private _http: HttpClient,
+    private _router : Router
+    ) { }
 
   _url = 'https://localhost:7085/api/user/'
 
+  login(loginForm : Login) : void {
+    this._http.post<ConnectedUser>(this._url+'login', loginForm).subscribe({
+      next : (res) => {
+        localStorage.setItem('token', res.token);
+        this._isConnected$.next(true);
+        this._router.navigateByUrl('/');
+      }
+    });
+  }
 
+  logout() : void {
+    localStorage.removeItem('token');
+    this._isConnected$.next(false);
+    this._router.navigateByUrl('/');
+
+  }
 }
